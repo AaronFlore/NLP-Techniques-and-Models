9bd22a286fe30a28b3f2f2704f756ce1fa590e46
Aaron Floreani <78060817+AaronFlore@users.noreply.github.com>
2024-05-17 13:10:07 -0700

    Initial commit

---
+++ Sentiment-Classifier-and-Text-Generator/lm.py
@@ -0,0 +1,331 @@
+"""
+Description: PS0
+Date: January 30th, 2024
+Author: Aaron Floreani
+"""
+
+import math
+import argparse
+import numpy as np
+import random
+
+import matplotlib
+import matplotlib.pyplot as plt
+
+from tqdm import tqdm
+from collections import defaultdict
+from nltk.tokenize import word_tokenize
+
+
+class LanguageModel:
+
+    def __init__(self, args):
+        """
+        A bigram language model with add-alpha smoothing.
+
+        Attributes
+        --------------------
+            alpha -- alpha for add-alpha smoothing
+            train_tokens -- list of training data tokens
+            val_tokens -- list of validation data tokens
+            vocab -- vocabulary frequency dict (key = word, val = frequency)
+            token_to_idx -- vocabulary index dict (key = word, val = index)
+            bigrams -- 2D np array of bigram probabilities, where each (i, j)
+                       value is the smoothed prob of the bigram starting with
+                       vocab token index i followed by vocab token index j
+        """
+        self.alpha = args.alpha
+        self.train_tokens = self.tokenize(args.train_file)
+        self.val_tokens = self.tokenize(args.val_file)
+        self.show_plot = args.show_plot
+
+        # Use only the specified fraction of training data.
+        num_samples = int(args.train_fraction * len(self.train_tokens))
+        self.train_tokens = self.train_tokens[: num_samples]
+        self.vocab = self.make_vocab(self.train_tokens)
+        self.token_to_idx = {word: i for i, word in enumerate(self.vocab)}
+        self.bigrams = self.compute_bigrams(self.train_tokens)
+
+    def get_indices(self, tokens):
+        """
+        Converts each of the string tokens to indices in the vocab.
+
+        Parameters
+        --------------------
+            tokens    -- list of tokens
+
+        Returns
+        --------------------
+            list of token indices in the vocabulary
+        """
+        return [self.token_to_idx[token] for token in tokens if token in self.token_to_idx]
+
+    def compute_bigrams(self, tokens):
+        """
+        Populates probability values for a 2D np array of all bigrams.
+
+        Parameters
+        --------------------
+            tokens    -- list of tokens
+            alpha     -- alpha for add-alpha smoothing
+
+        Returns
+        --------------------
+            bigrams   -- 2D np array of bigram probabilities, where each (i, j)
+                       value is the smoothed prob of the bigram starting with
+                       vocab token index i followed by vocab token index j
+        """
+        counts = np.zeros((len(self.vocab), len(self.vocab)), dtype=float)
+        probs = np.zeros((len(self.vocab), len(self.vocab)), dtype=float)
+        tokens = self.get_indices(tokens)
+
+        # Count up all the bigrams.
+        # Estimate bigram probabilities using the counts (and alpha).
+        # First, add alpha to each bigram count.
+        # Then, divide by the sum of the bigram counts + alpha times |V|.
+
+        for i in range(len(tokens) - 1):
+            counts[tokens[i]][tokens[i+1]] += 1
+
+        for i in range(len(self.vocab)):
+            total_count = np.sum(counts[i]) + self.alpha * len(self.vocab)
+            probs[i] = (counts[i] + self.alpha) / total_count
+
+        return probs
+
+    def compute_perplexity(self, tokens):
+        """
+        Evaluates the LM by calculating perplexity on the given tokens.
+
+        Parameters
+        --------------------
+            bigrams    -- 2D np array of bigram probabilities, where each (i, j)
+                       value is the smoothed prob of the bigram starting with
+                       vocab token index i followed by vocab token index j
+            tokens     -- list of tokens
+
+        Returns
+        --------------------
+            perplexity
+        """
+        tokens = self.get_indices(tokens)
+
+        # Sum up all the bigram log probabilities in the test corpus.
+        # Be sure to divide by the number of tokens, not the vocab size!
+
+        if not tokens:
+            print("Warning: Empty tokens list. Unable to calculate perplexity.")
+            return None 
+
+        bigrams = self.compute_bigrams(self.train_tokens)
+
+        log_prob_sum = 0
+        for i in range(len(tokens) - 1):
+            log_prob_sum += np.log(bigrams[tokens[i]][tokens[i+1]])
+
+        perplexity = np.exp(-log_prob_sum / len(tokens))
+        return perplexity
+
+    def tokenize(self, corpus):
+        """
+        Splits the given corpus file into tokens using nltk's tokenizer.
+
+        Parameters
+        --------------------
+            corpus    -- filename as a string
+
+        Returns
+        --------------------
+            tokens    -- list of tokens
+        """
+        #part a
+        with open(corpus, 'r') as file:
+            text = file.read()
+        return word_tokenize(text)
+
+    def make_vocab(self, train_tokens):
+        """
+        Creates a vocabulary dictionary that maps tokens to frequencies.
+
+        Parameters
+        --------------------
+            train_tokens    -- list of training tokens
+
+        Returns
+        --------------------
+            vocab           -- vocab frequency dict (key = word, val = freq)
+        """
+        vocab = defaultdict(int)
+        for token in train_tokens:
+            vocab[token] += 1
+        return vocab
+
+    def plot_vocab(self, vocab):
+        """
+        Plots words from most to least common, with frequency on the y-axis.
+
+        Parameters
+        --------------------
+            vocab           -- vocab frequency dict (key = word, val = freq)
+        """
+        sorted_vocab = sorted(vocab.items(), key=lambda x: x[1], reverse=True)
+        words, frequencies = zip(*sorted_vocab)
+        token_indices = list(range(len(words)))
+
+        plt.figure(figsize=(12, 6))
+        plt.plot(token_indices, frequencies, linestyle='-', color='b')
+        plt.xlabel('Tokens')
+        plt.ylabel('Frequency')
+        plt.title('Word Frequencies')
+
+        plt.show()
+    
+    def compute_ngrams(self, tokens, n):
+        """
+        Creates probability values for a 2D np array of all n-grams.
+
+        Parameters
+        --------------------
+            tokens    -- list of tokens
+            n         -- order of the n-grams
+
+        Returns
+        --------------------
+            ngrams    -- 2D np array of n-gram probabilities
+        """
+        counts = np.zeros(tuple([len(self.vocab)] * n), dtype=float)
+        probs = np.zeros(tuple([len(self.vocab)] * n), dtype=float)
+        tokens = self.get_indices(tokens)
+
+        for i in range(len(tokens) - n + 1):
+            indices = tuple(tokens[i:i+n])
+            counts[indices] += 1
+
+        for i in range(len(self.vocab)):
+            total_count = np.sum(counts[(i,) * n]) + self.alpha * len(self.vocab)**n
+            probs[(i,) * n] = (counts[(i,) * n] + self.alpha) / total_count
+
+        return probs
+
+    def generate_text(self, n, length=100):
+        """
+        Generate text in the style of the book using n-grams.
+
+        Parameters
+        --------------------
+            n         -- order of the n-grams
+            length    -- length of the generated text (number of tokens)
+
+        Returns
+        --------------------
+            generated_text -- list of generated tokens
+        """
+        if n < 1:
+            raise ValueError("n must be greater than or equal to 1.")
+
+        generated_text = []
+
+        current_word = random.choice(list(self.token_to_idx.keys()))
+
+        for _ in range(length):
+            next_word_probs = self.bigrams[self.token_to_idx[current_word]]
+            next_word_idx = np.random.choice(len(next_word_probs), p=next_word_probs)
+
+            next_word = list(self.token_to_idx.keys())[next_word_idx]
+            generated_text.append(next_word)
+            current_word = next_word
+
+        return generated_text
+
+
+def main(args):
+    lm = LanguageModel(args)
+
+    # Plot word frequencies by setting command-line arg show_plot.
+    if args.show_plot:
+        lm.plot_vocab(lm.vocab)
+
+    # Plot training and validation perplexities as a function of alpha.
+    alpha_values = [1e-5, 1e-4, 1e-3, 1e-2, 1e-1, 1, 10]
+    train_perplexities = []
+    val_perplexities = []
+
+    for alpha in alpha_values:
+        lm.alpha = alpha
+        train_perplexity = lm.compute_perplexity(lm.train_tokens)
+        val_perplexity = lm.compute_perplexity(lm.val_tokens)
+
+        train_perplexities.append(train_perplexity)
+        val_perplexities.append(val_perplexity)
+
+    plt.figure(figsize=(10, 5))
+    plt.semilogx(alpha_values, train_perplexities, marker='o', label='Training Perplexity')
+    plt.semilogx(alpha_values, val_perplexities, marker='o', label='Validation Perplexity')
+    plt.xlabel('Alpha')
+    plt.ylabel('Perplexity')
+    plt.title('Training and Validation Perplexities vs. Alpha')
+    plt.legend()
+
+    for (alpha, train_ppl, val_ppl) in zip(alpha_values, train_perplexities, val_perplexities):
+        plt.text(alpha, train_ppl, f'({alpha:.4f}, {train_ppl:.2f})', ha='right', va='bottom')
+        plt.text(alpha, val_ppl, f'({alpha:.4f}, {val_ppl:.2f})', ha='right', va='bottom')
+
+    plt.grid(True)
+    plt.tight_layout()
+    plt.show()
+
+    # Plot train/val perplexities for varying amounta of training data.
+    fractions = np.arange(0.1, 1.1, 0.1)
+    train_perplexities_fraction = []
+    val_perplexities_fraction = []
+
+    for fraction in fractions:
+        num_samples = int(fraction * len(lm.train_tokens))
+        lm.train_tokens = lm.train_tokens[:num_samples]
+
+        train_perplexity = lm.compute_perplexity(lm.train_tokens)
+        val_perplexity = lm.compute_perplexity(lm.val_tokens)
+
+        train_perplexities_fraction.append(train_perplexity)
+        val_perplexities_fraction.append(val_perplexity)
+
+    plt.figure(figsize=(10, 5))
+    plt.plot(fractions, train_perplexities_fraction, marker='o', label='Training Perplexity')
+    plt.plot(fractions, val_perplexities_fraction, marker='o', label='Validation Perplexity')
+    plt.xlabel('Fraction of Training Data')
+    plt.ylabel('Perplexity')
+    plt.title('Training and Validation Perplexities vs. Fraction of Training Data')
+    plt.legend()
+
+    for (fraction, train_ppl, val_ppl) in zip(fractions, train_perplexities_fraction, val_perplexities_fraction):
+        plt.text(fraction, train_ppl, f'({fraction:.2f}, {train_ppl:.2f})', ha='right', va='bottom', fontsize=5)
+        plt.text(fraction, val_ppl, f'({fraction:.2f}, {val_ppl:.2f})', ha='right', va='bottom', fontsize=5)
+
+    plt.grid(True)
+    plt.tight_layout()
+
+    plt.show()
+
+    # Generate frankenstein text using bigrams
+    # run with python lm.py --train_file frankenstein.txt --frankenstein_file frankenstein.txt
+    generated_text_bigram = lm.generate_text(n=2, length=100)
+    print("Generated Text (Bigram):", ' '.join(generated_text_bigram))
+
+    # Generate frankenstein text using trigrams
+    generated_text_trigram = lm.generate_text(n=3, length=100)
+    print("Generated Text (Trigram):", ' '.join(generated_text_trigram))
+
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('--train_file', default='lm-data/brown-train.txt')
+    parser.add_argument('--frankenstein_file', default='frankenstein.txt')
+    parser.add_argument('--val_file', default='lm-data/brown-val.txt')
+    parser.add_argument('--train_fraction', type=float, default=1.0, help='Specify a fraction of training data to use to train the language model.')
+    parser.add_argument('--alpha', type=float, default=0.0001, help='Parameter for add-alpha smoothing.')
+    parser.add_argument('--show_plot', type=bool, default=False, help='Whether to display the word frequency plot.')
+
+    args = parser.parse_args()
+    main(args)
---
+++ Sentiment-Classifier-and-Text-Generator/sentiment_classifier.py
@@ -0,0 +1,229 @@
+"""
+Description: PS0
+Date: January 30th, 2024
+Author: Aaron Floreani
+"""
+
+import argparse
+import numpy as np
+import nltk
+nltk.download('stopwords')
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+
+from tqdm import tqdm
+from collections import defaultdict
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.linear_model import LogisticRegression
+
+
+class BaselineClassifier():
+
+    def __init__(self, args):
+        """
+        A baseline classifier that always predicts positive sentiment.
+
+        Attributes
+        --------------------
+            train_sents -- list of training sentences as strings
+            train_labels -- list of training integer (0 or 1) labels
+            val_sents -- list of validation sentences as strings
+            val_labels -- list of validation integer (0 or 1) labels
+
+        """
+        self.remove_stopwords = args.remove_stopwords  
+        self.train_sents, self.train_labels = self.read_data(args.train_file, remove_stopwords=self.remove_stopwords)  
+        self.val_sents, self.val_labels = self.read_data(args.val_file, remove_stopwords=self.remove_stopwords)
+
+    def read_data(self, filename, remove_stopwords=False):
+        """
+        Extracts all the sentences and labels from the input file.
+
+        Parameters
+        --------------------
+            filename    -- filename as a string
+
+        Returns
+        --------------------
+            sents       -- list of sentences as strings
+            labels      -- list of integer (0 or 1) labels
+        """
+        sents = []
+        labels = []
+        with open(filename) as f:
+            for line in f.readlines():
+                line = line.strip().split(' ', 1)  # only split once
+                sentence = line[1]
+
+                if remove_stopwords:
+                    stop_words = set(stopwords.words('english'))
+                    word_tokens = word_tokenize(sentence)
+                    sentence = ' '.join([w for w in word_tokens if not w in stop_words])
+
+                sents.append(sentence)
+                labels.append(int(line[0]))
+        return sents, labels
+
+    def predict(self, corpus):
+        """
+        Always predicts a value of 1 given the input corpus.
+
+        Parameters
+        --------------------
+            corpus    -- list of sentences
+
+        Returns
+        --------------------
+            list of 1 for each sentence in the corpus
+        """
+
+        return [1] * len(corpus)
+
+    def evaluate(self):
+        """
+        Computes and prints accuracy on training and validation predictions.
+        """
+
+        train_predictions = self.predict(self.train_sents)
+        val_predictions = self.predict(self.val_sents)
+
+        train_accuracy = np.mean(np.array(train_predictions) == np.array(self.train_labels))
+        val_accuracy = np.mean(np.array(val_predictions) == np.array(self.val_labels))
+
+        print(f"Training Accuracy: {train_accuracy:.2%}")
+        print(f"Validation Accuracy: {val_accuracy:.2%}")
+
+
+class NaiveBayesClassifier(BaselineClassifier):
+
+    def __init__(self, args):
+        """
+        An sklearn Naive Bayes classifier with unigram features.
+
+        Attributes
+        --------------------
+            train_sents -- list of training sentences as strings
+            train_labels -- list of training integer (0 or 1) labels
+            val_sents -- list of validation sentences as strings
+            val_labels -- list of validation integer (0 or 1) labels
+            vectorizer -- sklearn CountVectorizer for training data unigrams
+            classifier -- sklearn MultinomialNB classifer object
+
+        """
+        super().__init__(args)
+
+        self.vectorizer = CountVectorizer(stop_words='english')
+        self.classifier = MultinomialNB()
+        self.train()
+
+    def train(self):
+        """
+        Trains a Naive Bayes classifier on training sentences and labels.
+        """
+
+        X_train = self.vectorizer.fit_transform(self.train_sents)
+        y_train = np.array(self.train_labels)
+        self.classifier.fit(X_train, y_train)
+
+    def predict(self, corpus):
+        """
+        Predicts labels on the corpus using the trained classifier.
+
+        Parameters
+        --------------------
+            corpus    -- list of sentences
+
+        Returns
+        --------------------
+            a list of predictions
+        """
+
+        X_corpus = self.vectorizer.transform(corpus)
+        predictions = self.classifier.predict(X_corpus)
+        return predictions
+
+
+class LogisticRegressionClassifier(NaiveBayesClassifier):
+
+    def __init__(self, args):
+        """
+        An sklearn Logistic Regression classifier with unigram features.
+
+        Attributes
+        --------------------
+            train_sents -- list of training sentences as strings
+            train_labels -- list of training integer (0 or 1) labels
+            val_sents -- list of validation sentences as strings
+            val_labels -- list of validation integer (0 or 1) labels
+            vectorizer -- sklearn CountVectorizer for training data unigrams
+            classifier -- sklearn LogisticRegression classifer object
+
+        """
+        BaselineClassifier.__init__(self, args)
+
+        self.vectorizer = CountVectorizer()
+        self.classifier = LogisticRegression(solver=args.solver, penalty=args.penalty, C=args.C)
+        self.train()
+
+
+class BigramLogisticRegressionClassifier(LogisticRegressionClassifier):
+
+    def __init__(self, args):
+        """
+        A Logistic Regression classifier with unigram and bigram features.
+
+        Attributes
+        --------------------
+            train_sents -- list of training sentences as strings
+            train_labels -- list of training integer (0 or 1) labels
+            val_sents -- list of validation sentences as strings
+            val_labels -- list of validation integer (0 or 1) labels
+            vectorizer -- sklearn CountVectorizer for unigrams and bigrams
+            classifier -- sklearn LogisticRegression classifer object
+
+        """
+        BaselineClassifier.__init__(self, args)
+
+        self.vectorizer = CountVectorizer(ngram_range=(1, 2))
+        self.classifier = LogisticRegression(solver=args.solver, penalty=args.penalty, C=args.C)
+        self.train()
+
+
+def main(args):
+    # Evaluate basline classifier (i.e., always predicts positive).
+    print("Baseline Evaluation:")
+    baseline_classifier = BaselineClassifier(args)
+    baseline_classifier.evaluate()
+
+    # Evaluate Naive Bayes classifier with unigram features.
+    print("Naive Bayes Evaluation:")
+    nb_classifier = NaiveBayesClassifier(args)
+    nb_classifier.evaluate()
+
+    # Evaluate logistic regression classifier with unigrams.
+    print("Logistic Regression Evaluation:")
+    lr_classifier = LogisticRegressionClassifier(args)
+    lr_classifier.evaluate()
+
+    # Evaluate logistic regression classifier with unigrams + bigrams.
+    print("Bigram Logistic Regression Evaluation:")
+    bigram_lr_classifier = BigramLogisticRegressionClassifier(args)
+    bigram_lr_classifier.evaluate()
+
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+
+    # argument to remove stop words
+    parser.add_argument('--remove_stopwords', action='store_true', help='Remove stopwords from sentences.')
+
+    parser.add_argument('--train_file', default='sentiment-data/train.txt')
+    parser.add_argument('--val_file', default='sentiment-data/val.txt')
+    parser.add_argument('--solver', default='liblinear', help='Optimization algorithm.')
+    parser.add_argument('--penalty', default='l2', help='Regularization for logistic regression.')
+    parser.add_argument('--C', type=float, default=1.0, help='Inverse of regularization strength for logistic regression.')
+
+    args = parser.parse_args()
+    main(args)
